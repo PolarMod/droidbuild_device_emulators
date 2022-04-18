82cf6435d36493c69583641f5ea831b209048f12
Andrewerr <p01ar@p01ar.net>
2022-04-18 19:26:33 +0300

    [droidbuild] [emulator]: fix arch name

--- emulator-x86_64-microg/Droidbuild.sh
+++ emulator-x86_64-microg/Droidbuild.sh
@@ -1,6 +1,6 @@
-setup_target emulator-x86
+setup_target emulator-x86_64-microg
 
-target_emulator-x86(){
+target_emulator-x86_64-microg(){
     LOCAL_PATH=$(dirname $BASH_SOURCE)
     # Clean build variables
     include $BASEDIR/droidbuild/build_vars.sh
@@ -8,13 +8,17 @@ target_emulator-x86(){
     # Set device bitness
     TARGET_ARCH_BITNESS=64
     # Set architecture for emulator
-    TARGET_ARCH="x86"
+    TARGET_ARCH="x86_64"
     # Codename of device
-    TARGET_CODENAME="emulator-x86"
+    TARGET_CODENAME="emulator-x86_64-microg"
     # Fully qualified model name
-    TARGET_FULLNAME="Emulator(x86)"
+    TARGET_FULLNAME="Emulator(x86_64) with microG"
     # Build type
     TARGET_BUILDTYPE="eng"
+    # Set hosts category(allows communication with GMS)
+    TARGET_HOSTS_CAT=2
+    # Include microG in the build
+    TARGET_USES_MICROG=1
     # Call build
     target_build-emulator
 }
--- emulator-x86_64/Droidbuild.sh
+++ emulator-x86_64/Droidbuild.sh
@@ -1,6 +1,6 @@
-setup_target emulator-x86-microg
+setup_target emulator-x86_64
 
-target_emulator-x86-microg(){
+target_emulator-x86(){
     LOCAL_PATH=$(dirname $BASH_SOURCE)
     # Clean build variables
     include $BASEDIR/droidbuild/build_vars.sh
@@ -8,17 +8,13 @@ target_emulator-x86-microg(){
     # Set device bitness
     TARGET_ARCH_BITNESS=64
     # Set architecture for emulator
-    TARGET_ARCH="x86"
+    TARGET_ARCH="x86_64"
     # Codename of device
-    TARGET_CODENAME="emulator-x86-microg"
+    TARGET_CODENAME="emulator-x86_64"
     # Fully qualified model name
-    TARGET_FULLNAME="Emulator(x86) with microG"
+    TARGET_FULLNAME="Emulator(x86_64)"
     # Build type
     TARGET_BUILDTYPE="eng"
-    # Set hosts category(allows communication with GMS)
-    TARGET_HOSTS_CAT=2
-    # Include microG in the build
-    TARGET_USES_MICROG=1
     # Call build
     target_build-emulator
 }
