86ef2e23efade57b8d50080cb335dd42b43f21b2
Andrewerr <p01ar@p01ar.net>
2022-04-19 18:13:07 +0300

    [droidbuild]: added x86 emulator with microG

---
+++ emulator-x86-microg/Droidbuild.sh
@@ -0,0 +1,29 @@
+setup_target emulator_x86_microg
+
+target_emulator_x86_64_microg(){
+    LOCAL_PATH=$(dirname $BASH_SOURCE)
+    # Clean build variables
+    include $BASEDIR/droidbuild/build_vars.sh
+    TARGET_NEEDS_RESYNC=false
+    # Set device bitness
+    TARGET_ARCH_BITNESS=32
+    # Set architecture for emulator
+    TARGET_ARCH="x86"
+    # Codename of device
+    TARGET_CODENAME="emulator-x86-microg"
+    # Fully qualified model name
+    TARGET_FULLNAME="Emulator(x86) with microG"
+    # Build type
+    TARGET_BUILDTYPE="eng"
+    # Set hosts category(allows communication with GMS)
+    TARGET_HOSTS_CAT=2
+    # Include microG in the build
+    TARGET_USES_MICROG=1
+    # Call build
+    target_build-emulator
+}
+
+# Currently dummy droidbuild target is required
+droidbuild_module(){
+    echo 1 > /dev/null 
+}
